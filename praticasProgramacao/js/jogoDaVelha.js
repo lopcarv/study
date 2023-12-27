ae488d81e8f4c7bedf6bfff6585b537f58883f60
lopcarv <d7m7e7@gmail.com>
2023-12-27 18:40:26 -0300

    Commit 27/12/2023

---
+++ praticasProgramacao/js/jogoDaVelha.js
@@ -0,0 +1,24 @@
+let tabuleiro
+let board
+let aviso
+let jogador
+let linha
+let coluna
+
+function iniciar(){
+    tabuleiro = []
+    board = document.getElementById('board')
+
+}
+
+function exibir(){
+    
+}
+
+function jogar(){
+    
+}
+
+function checar(){
+    
+}
