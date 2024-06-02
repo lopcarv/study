05280415b43e40d9839d684d5d918d955ecd7da3
Luis Orlando <d7m7e7@gmail.com>
2024-06-02 12:43:03 -0300

    Add exercicios de Python

---
+++ python/conceitos.py
@@ -0,0 +1,14 @@
+print("Hello World")
+print("poo")
+
+# string = texto literal
+nome = "Luis Orlando"
+
+# Inteiro
+idade = 20
+
+print(nome)
+print(idade)
+
+nomeDigitado = input("Por gentileza, digite um nome:  ")
+print("O nome digitado foi: ", nomeDigitado)
