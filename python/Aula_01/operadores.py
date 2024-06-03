90ea03c8fc543969bff3bd288d22dbefbaa440c6
Luis Orlando <d7m7e7@gmail.com>
2024-06-03 17:22:38 -0300

    Add exercicios if

---
+++ python/Aula_01/operadores.py
@@ -0,0 +1,32 @@
+# Professor: Orion Teles
+n1 = 7
+n2 = 9
+aprovado = True
+media = 6
+minimo = 7
+
+print("operadores aritmeticos")
+print("Soma: ", n1 + n2)
+print("Subtração: ", n1 - n2)
+print("Multiplicação: ", n1 * n2)
+print("Divisão: ", n1 / n2)
+print("Resto: ", n1 % n2)
+
+print()
+print("Operadores Relacionais")
+print("Igualdade: ", n1 == n2)
+print("Diferença: ", n1 != n2)
+print("Menor que: ", n1 < n2)
+print("Maior que: ", n1 > n2)
+print("Maior ou igual: ", n1 >= n2)
+print("Menor que: ", n1 <= n2)
+
+print()
+print("operadores lógicos" )
+print(aprovado)
+print(not aprovado)
+print(n1 >= 7 and minimo == 7)
+print(n1 > 6 or minimo == 7)
+
+
+
---
+++ python/Aula_02/if.py
@@ -0,0 +1,9 @@
+n1 = 10
+n2 = 4
+n3 = 6
+n4 = 2
+
+soma = n1 + n2 + n3 + n4
+media = soma / 4
+
+print(media)
