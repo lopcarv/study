bd43d353a623c0a4832a992339cd41a22490b82c
Luis Orlando <d7m7e7@gmail.com>
2024-06-19 16:13:43 -0300

    Add exercicios 190624

--- python/Aula_02/if.py
+++ python/Aula_02/if.py
@@ -1,9 +1,16 @@
-n1 = 10
-n2 = 4
+n1 = 4
+n2 = 6
 n3 = 6
-n4 = 2
+n4 = 4
 
 soma = n1 + n2 + n3 + n4
 media = soma / 4
 
-print(media)
+print("A média é: ",media)
+
+if media >=7:
+    print("aprovado")
+elif media < 5:
+    print("Reprovado")
+else:
+    print("Recuperação !")
---
+++ python/udemy/aula_03.py
@@ -0,0 +1,26 @@
+print(12, 34, 1011, sep="-", end='\n##' )
+print(56, 78, sep="-", end='##\n')
+print(9, 10, sep='_')
+
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Luiz Otávio')
+print(1, 'Luiz "Otávio"')
+
+# Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'")
+
+# Escape
+print("Luiz \"Otávio\"")
+
+# r
+print(r"Luiz \"Otávio\"")
