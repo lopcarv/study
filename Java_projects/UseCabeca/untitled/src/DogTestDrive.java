dd00732cd13839e01dc67a252f64b21789df650d
lopcarv <d7m7e7@gmail.com>
2025-02-11 00:53:41 -0300

    11Fev2025|12:53pm

---
+++ Java_projects/UseCabeca/untitled/src/DogTestDrive.java
@@ -0,0 +1,8 @@
+public class DogTestDrive {
+    public static void main (String[] args) {
+        // o codigo que teste de Dog entra aqui
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
